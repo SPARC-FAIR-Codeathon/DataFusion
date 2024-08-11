b119d4550fe746eb5c9ebaca3e2f4769b3c6c27f
Pranjal Garg <58928080+neurogarg@users.noreply.github.com>
2024-08-11 16:50:12 +0530

    Update execute.sh

--- service.cli/execute.sh
+++ service.cli/execute.sh
@@ -63,16 +63,17 @@ fi
 
 # Launch data retrieval and analysis
 echo "Starting data retrieval"
-git clone https://github.com/neurogarg/DataWave.git
-cd DataWave
-python3 main.py \
-    -i "$dat_filen" \
-    -i "$analysis_filen" \
-    -i "$json_filen"
+cd ..
+cd datawave
+
+python3 main.py  \
+  -i "$dat_filen" \
+  -i "$analysis_filen" \
+  -i "$json_filen"
+
 echo "Analysis and retrieval completed successfully, adding it to the output..."
 # Add derived_files to output
 
 zip -r "${OUTPUT_FOLDER}"/derived_files.zip "derived_file.csv" "sample_plot.png" "analysis_plot.png"
-
 
 
