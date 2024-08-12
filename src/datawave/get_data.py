bd34aa5783927311907a8c92650547e80b5a7134
Pranjal Garg <58928080+neurogarg@users.noreply.github.com>
2024-08-12 11:38:53 +0530

    Update get_data.py

--- src/datawave/get_data.py
+++ src/datawave/get_data.py
@@ -1,7 +1,8 @@
 import os
 import utils
 import mat_file
-
+import adicht_file
+import rhd_file
 
 def run(data_file_ext, data_file, field_name_dictionary):
     supported_ext = [".mat", ".nwb", ".csv", ".txt", ".adicht", ".rhd"]
@@ -9,5 +10,13 @@ def run(data_file_ext, data_file, field_name_dictionary):
     if data_file_ext == supported_ext[0]:
         series, time_data = mat_file.read_mat(data_file, field_name_dictionary)
         df = utils.process_time_series(series, time_data)
+
+    if data_file_ext == supported_ext[4]: ## adicht
+        series, time_data = adicht_file.read_adi_file(data_file, field_name_dictionary)
+        df = utils.process_time_series(series, time_data)
+
+    if data_file_ext == supported_ext[5]: ## rhd
+        df = rhd_file.run(data_file)
+
 
     return df
