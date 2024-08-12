585355ac03999284ef59a450709f52d704533117
Pranjal Garg <58928080+neurogarg@users.noreply.github.com>
2024-08-12 14:44:02 +0530

    Update get_data.py

--- src/datawave/get_data.py
+++ src/datawave/get_data.py
@@ -3,13 +3,20 @@ import utils
 import mat_file
 #import adicht_file
 import rhd_file
+import pandas as pd
 
 def run(data_file_ext, data_file, field_name_dictionary):
     supported_ext = [".mat", ".nwb", ".csv", ".txt", ".adicht", ".rhd"]
 
     if data_file_ext == supported_ext[0]:
         series, time_data = mat_file.read_mat(data_file, field_name_dictionary)
-        df = utils.process_time_series(series, time_data)
+        if (series is not None) and (time_data is not None):
+            df = utils.process_time_series(series, time_data)
+            df = mat_file.add_other_labels(mat_file, field_name_dictionary, df)
+        else:
+            df = pd.DataFrame({"Time": "empty", "Series": "empty" })
+
+        mat_file.convert_mat_to_h5(data_file)
 
     ## Add linux implementation in future
     # if data_file_ext == supported_ext[4]: ## adicht
@@ -18,6 +25,7 @@ def run(data_file_ext, data_file, field_name_dictionary):
 
     elif data_file_ext == supported_ext[5]: ## rhd
         df = rhd_file.run(data_file)
+        rhd_file.rhd_to_nwb(data_file)
 
 
     return df
