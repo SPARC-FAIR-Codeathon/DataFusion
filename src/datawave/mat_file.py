5fdd3f694773dbc0e3b19d781bd7103d4c0cad62
Pranjal Garg <58928080+neurogarg@users.noreply.github.com>
2024-08-13 05:44:08 +0530

    Update mat_file.py

--- src/datawave/mat_file.py
+++ src/datawave/mat_file.py
@@ -4,6 +4,11 @@ import os
 import shutil
 
 def process_mat_file(mat, field_name_dictionary):
+    """
+    :param mat- loaded mat file
+    :param field_name_dictionary- field name dictionary
+    :return series and time data based on field name dictionary and create pseudo time stamps wherever applicable
+    """
     if "Time Series" in field_name_dictionary.keys():
         series_key = field_name_dictionary["Time Series"]
 
@@ -15,7 +20,7 @@ def process_mat_file(mat, field_name_dictionary):
 
             return series, time_data
 
-
+        ## if sampling frequency and starting time is given use that
         elif "Sampling Frequency" in field_name_dictionary.keys() and "Start Time" in field_name_dictionary.keys():
             sf_key = field_name_dictionary["Sampling Frequency"]
             sf = mat[sf_key]
@@ -44,6 +49,12 @@ def process_mat_file(mat, field_name_dictionary):
 
 
 def add_other_labels(data_file, field_name_dictionary, df):
+    """
+    Adds extra labels such as behavioural for machine learning and other things
+    :param data_file- mat data file name
+    :param field_name_dictionary- field name dictionaries
+    :param df- data frame containing time series and time stamps
+    """
 
     if field_name_dictionary:  ## only proceed if it is not empty
         mat = scipy.io.loadmat(data_file) 
@@ -58,11 +69,19 @@ def add_other_labels(data_file, field_name_dictionary, df):
 
 
 def read_mat(data_file, field_name_dictionary):
+    """
+    return the loaded mat file
+    :param data_file- the name of the mat file
+    :param field_name_dictionary- field name dictionary from JSON containing the field names to be stored in csv
+    """
     mat = scipy.io.loadmat(data_file)
     return process_mat_file(mat, field_name_dictionary)
 
 
 def convert_mat_to_h5(data_file):
+    """
+    convert the data_file to h5 and stores it in correct location as extra_derived_files
+    """
     command = f"python3 mat2h5.py {data_file}"
     os.system(command)
     
