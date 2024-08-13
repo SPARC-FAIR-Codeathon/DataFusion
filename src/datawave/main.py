da2e2eab80204727186484c0dc27ff0cbe1e12da
Pranjal Garg <58928080+neurogarg@users.noreply.github.com>
2024-08-13 05:36:56 +0530

    Update main.py

--- src/datawave/main.py
+++ src/datawave/main.py
@@ -7,11 +7,13 @@ import run_analysis_file
 import pandas as pd
 
 def read_json(file_path):
+    """
+    reads the JSON file and returns it as a dictionary
+    :param file_path- name of the JSON file
+    """
     with open(file_path, 'r') as f:
         field_name_dict = json.load(f)
     return dict(field_name_dict)
-
-
 
 parser = argparse.ArgumentParser(description='Data retrieval and analysis.')
 
@@ -34,18 +36,20 @@ supported_ext = [".mat", ".nwb", ".csv", ".txt", ".adicht", ".rhd"]
 # Extract the file extension
 data_file_ext = os.path.splitext(data_file)[1].lower()
 
-# Ensure data file has a supported extension
+# Ensure the data file has a supported extension
 if data_file_ext not in supported_ext:
     raise ValueError(f"Unsupported data file extension: {data_file_ext}")
 
+## Get field names from JSON and if JSON doesn't exist make an empty dictionary
 try:
     field_name_dict = read_json(json_file)
 except Exception as e:
-    print(f"No json file found: {e}")
+    print(f"No JSON file found: {e}")
     field_name_dict = {}
 
 get_final_df = get_data.run(data_file_ext, data_file, field_name_dict)
 
+## if df is returned as None by get_data then create a empty data frame
 if get_final_df is None:
     get_final_df = pd.DataFrame()
 
@@ -54,5 +58,5 @@ csv_file_path = "derived_file.csv"
 get_final_df.to_csv(csv_file_path, index=False)
 
 
-## run python analysis code
+## run python analysis code if applicable
 run_analysis_file.run(python_file, get_final_df)
